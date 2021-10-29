e508fae82bf1c6143a85215dc904a84e764abdfb
kinsayeth <sourcing>
2021-10-29 17:58:36 +0800

    Incomplete module 7 quiz, encountered issue.

---
+++ gdstruc_module7_quiz/src/com/gdstruc/module7/Main.java
@@ -0,0 +1,20 @@
+package com.gdstruc.module7;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Tree tree = new Tree();
+
+        tree.insert(25);
+        tree.insert(9);
+        tree.insert(78);
+        tree.insert(5);
+        tree.insert(-21);
+        tree.insert(-55);
+        tree.insert(1);
+
+        //tree.traverseInOrder();
+        System.out.println(tree.get());
+    }
+}
---
+++ gdstruc_module7_quiz/src/com/gdstruc/module7/Tree.java
@@ -0,0 +1,26 @@
+package com.gdstruc.module7;
+
+public class Tree {
+
+    private Node root;
+
+    public void insert(int value)
+    {
+        if (root == null)
+        {
+            root = new Node(value);
+        }
+        else
+        {
+            root.insert(value);
+        }
+    }
+
+    public void traverseInOrder()
+    {
+        if (root != null)
+        {
+            root.traverseInOrder();
+        }
+    }
+}
